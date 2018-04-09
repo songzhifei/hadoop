6f910d8281c043a73a1ea110f70dc0f06487a6f5
songzhifei <zhifeisong@163.com>
2018-04-09 22:17:04 +0800

    1

--- src/main/java/cn/mrsong/hadoop/hbase/HbaseDemo.java
+++ src/main/java/cn/mrsong/hadoop/hbase/HbaseDemo.java
@@ -32,8 +32,8 @@ public class HbaseDemo {
 	@Before
 	public void init() throws Exception {
 		conf = HBaseConfiguration.create();
-		//conf.set("hbase.zookeeper.quorum","itcast03:2181,itcast04:2181");
-		conf.set("hbase.zookeeper.quorum","itcast02:2181,itcast03:2181");
+		conf.set("hbase.zookeeper.quorum","itcast03:2181,itcast04:2181");
+		//conf.set("hbase.zookeeper.quorum","itcast02:2181,itcast03:2181");
 		//conf.set("hbase.zookeeper.property.clientPort","2181");
 		//conf.set("hbase.master", "http://itcast03:2181:60010");
 	}
@@ -53,7 +53,7 @@ public class HbaseDemo {
 		HTable table = new HTable(conf, "people");
 		List<Put> puts = new ArrayList<Put>(1000);
 		for(int i=1;i<10000;i++) {
-			Put put = new Put(Bytes.toBytes("kr"+i));
+			Put put = new Put(Bytes.toBytes("kr0"+i));
 			put.add(Bytes.toBytes("info"), Bytes.toBytes("money"), Bytes.toBytes(1000+i));
 			puts.add(put);
 			if(i % 1000 == 0 ) {
@@ -70,20 +70,21 @@ public class HbaseDemo {
 		Get get = new Get(Bytes.toBytes("kr0001"));
 		Result result = table.get(get);
 		table.close();
-	  	//String string =	result.getFamilyMap(Bytes.toBytes("data")).toString();
-	  	System.out.println(result.getValue(Bytes.toBytes("data"), Bytes.toBytes(1)));
+	  	byte[] bs =result.getValue(Bytes.toBytes("data"), Bytes.toBytes("money"));
+	  	System.out.println(Bytes.toInt(bs));
 	} 
 	@Test
 	public void test_getAll() throws Exception{
 		HTable table = new HTable(conf,"people");
 		List<Get> gets = new ArrayList<Get>();
 		for(int i=1;i<100;i++) {
-			Get get = new Get(Bytes.toBytes("kr"+i));
+			Get get = new Get(Bytes.toBytes("kr0"+i));
 			gets.add(get);
 		}
 		Result[] results = table.get(gets);
 		for (Result result : results) {
-			System.out.println(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("money")));
+			byte[] b = result.getValue(Bytes.toBytes("info"), Bytes.toBytes("money"));
+			System.out.println(Bytes.toInt(b));
 		}
 		table.close();
 	} 
